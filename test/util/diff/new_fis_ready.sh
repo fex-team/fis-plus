042a3e6995ac1f1933a415d1642f5e97bdb817dc
tianlili <tianlili@baidu.com>
2014-10-16 14:47:51 +0800

    [change]install a package

--- test/util/diff/new_fis_ready.sh
+++ test/util/diff/new_fis_ready.sh
@@ -8,4 +8,5 @@ npm install
 npm install fis-preprocessor-inline
 npm install fis-preprocessor-image-set
 npm install fis-postpackager-ext-map
+npm install fis-packager-autopack
 sh ${TEST_PATH}/release$1.sh new
